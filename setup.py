e3a9da689352b3532ec942dd72b86bf17f398ccd
Marc Brinkmann <git@marcbrinkmann.de>
2015-09-04 18:11:40 +0200

    Changed structure to full-fledge python package.

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='repl',
+    version='1.0.dev1',
+    description='Run command as REPL-environment (useful for git!).',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='https://github.com/mbr/repl',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['pyxdg'],
+    entry_points={
+        'console_scripts': [
+            'repl = repl:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ])

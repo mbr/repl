391801c5b32bc2743ad9c57655493b404a77d42d
Marc Brinkmann <git@marcbrinkmann.de>
2021-01-03 15:17:45 +0100

    Added a `default.nix` file to make the package installable on NixOS

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ pkgs ? (import <nixpkgs>) { } }:
+let
+  pyDeps = python-packages: with python-packages; [ click ];
+  python = pkgs.python3.withPackages pyDeps;
+in python.pkgs.buildPythonPackage {
+  name = "repl";
+  version = "1.1.dev1";
+  src = ./.;
+}
